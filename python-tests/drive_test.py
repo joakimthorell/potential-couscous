0a74a6e41af77dcc69a5faf962679e51ece43777
Philip <nordp@student.chalmers.se>
2017-09-13 16:04:19 +0200

    Added python test scripts for driving and sensor output

---
+++ python-tests/drive_test.py
@@ -0,0 +1,25 @@
+import nav as n
+from nav import *
+from nav1 import whole4, pause, cont
+from driving import stop, drive, steer
+init()
+
+time.sleep(1)
+
+g.limitspeed = None
+
+while(1>0):
+        drive(100)
+        time.sleep(2)
+        drive(0)
+        steer(100)
+        time.sleep(1)
+        steer(-100)
+        time.sleep(2)
+        drive(-100)
+        time.sleep(2)
+	drive(0)
+        steer(100)
+        sleep(1)
+        steer(-100)
+        time.sleep(2)
---
+++ python-tests/sensortest_distance.py
@@ -0,0 +1,18 @@
+import run as r
+import pygame
+from run import *
+
+#pygame.init()
+#pygame.mixer.init()
+#myList = [100]
+
+#for x in range(0, 99):
+#       myList[x] = pygame.mixer.Sound(str(x) + ".wav")
+#
+#myList[0].play()
+#myList[67].play()
+#myList[82].play()
+
+while True:
+        print(g.can_ultra)
+        time.sleep(1)
