850944558b8c5c0a6d9db1f40c56e2c1172e0042
skarrman <simonkarrman@icloud.com>
2017-09-20 12:21:19 +0200

    Change simulation dist and set constants to 1

--- python-tests/acc_vol1.py
+++ python-tests/acc_vol1.py
@@ -4,9 +4,11 @@ minDist = 0.5
 currentDist = 1.0
 goalVelocity = 30
 currentSpeed = goalVelocity
-k1 = 0.5
-k2 = 0.1
+k1 = 1
+k2 = 1
 k3 = 1
+
+hostVelocity = 20;
 
 timeInterval = 0.01
 
@@ -17,7 +19,7 @@ def p ( _curDist ):
 	return k1 * (_curDist - minDist)
 
 def integral (_lastIntegralSum, _curDist): 
-	totIntegral = _lastIntegralSum + (_curDist - minDist)/timeInterval
+	totIntegral = _lastIntegralSum + (_curDist - minDist) * timeInterval
 	return k2 * totIntegral
 
 def deriv (_currentDist, _lastErr) :
@@ -30,20 +32,24 @@ while True:
 
     lastErr = currentDist - minDist
 
-    totIntegral = totIntegral + (currentDist - minDist)/timeInterval
+    totIntegral = totIntegral + (currentDist - minDist) * timeInterval
 
-    speed = output
+    speed = currentSpeed + output
 
     if speed > goalVelocity:
         currentSpeed = goalVelocity
+    elif speed < 0:
+        currentSpeed = 0;
     else:
         currentSpeed = speed
 
     print ("Output " + str(output))
     print ("Speed " + str(currentSpeed))
     print ("Dist " + str(currentDist))
+    print ("Diff " +  str(lastErr))
+    print ("")
 
-    currentDist = currentDist - 0.001 * currentSpeed
+    currentDist = currentDist - (currentSpeed - hostVelocity)*timeInterval
 
     time.sleep(timeInterval)
 
