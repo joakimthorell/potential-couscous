96c0d8eeaf78fc3ea000c5d8332e233b006250e5
antonhager <anthage@student.chalmers.se>
2017-10-02 16:46:29 +0200

    added some pythonfiles for the main scripts

---
+++ scripts/main/inData.py
@@ -0,0 +1,2 @@
+class inData:
+	pass
---
+++ scripts/main/main.py
@@ -0,0 +1,10 @@
+import nav as n
+from nav import *
+from nav1 import whole4, pause, cont
+from driving import stop, drive, steer
+init()
+
+import time
+import SensorHandler
+
+print("hello")
