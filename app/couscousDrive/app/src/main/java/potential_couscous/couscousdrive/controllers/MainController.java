a3a22181c4c1af9d51710e7feb7ce06fbc26ebdf
annekeller <annekeller93@gmail.com>
2017-10-04 16:16:26 +0200

    small fix

--- app/couscousDrive/app/src/main/java/potential_couscous/couscousdrive/controllers/MainController.java
+++ app/couscousDrive/app/src/main/java/potential_couscous/couscousdrive/controllers/MainController.java
@@ -21,19 +21,20 @@ public class MainController {
 
     public void setToggleButtonListener(final ToggleGroup toggleGroup) {
             toggleGroup.setOnCheckedChangeListener(new ToggleGroup.OnCheckedChangeListener() {
+                CarCom carCom = MainActivity.getCarCom();
                 @Override
                 public void onCheckedChanged(ToggleGroup group, @IdRes int[] checkedId) {
                     switch (toggleGroup.getCheckedId()) {
                         case (R.id.manual_button):
-                            //carCom.sendData(carCom.MANUAL_KEY);
+                            carCom.sendData(carCom.MANUAL_KEY);
                             System.out.println("Manuell");
                             break;
                         case(R.id.acc_button):
-                            //carCom.sendData(carCom.ACC_KEY);
+                            carCom.sendData(carCom.ACC_KEY);
                             System.out.println("ACC");
                             break;
                         case(R.id.platoon_button):
-                            //carCom.sendData(carCom.PLATOON_KEY);
+                            carCom.sendData(carCom.PLATOON_KEY);
                             System.out.println("Kolonn");
                             break;
                         default:
